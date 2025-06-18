892f7648602db54ed9c7fd7d65076aee4c11aebb
smit-darji <smit.softvan@gmail3.com>
2025-06-18 18:14:30 +0530

    added

--- a.py
+++ a.py
@@ -1,5 +1,5 @@
 import snowflake.connector
-
+import os
 # Snowflake connection parameters
 conn = snowflake.connector.connect(
     user='yogeshmakwana',
@@ -8,21 +8,46 @@ conn = snowflake.connector.connect(
     warehouse='COMPUTE_WH'
 )
 
+
+def get_connection():
+    """
+    Establishes and returns a Snowflake database connection using environment variables.
+    """
+    user = os.getenv("SNOWFLAKE_USERNAME")
+    password = os.getenv("SNOWFLAKE_PASSWORD")
+    account = os.getenv("SNOWFLAKE_ACCOUNT")
+    warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
+    private_key_file = os.getenv("SNOWFLAKE_PRIVATE_KEY_FILE")
+    private_file_password = os.getenv("SNOWFLAKE_PRIVATE_KEY_PASSWORD")
+
+    return snowflake.connector.connect(
+        user=user,
+        authenticator="SNOWFLAKE_JWT",
+        private_key_file=private_key_file,
+        private_key_file_pwd=private_file_password,
+        account=account,
+        warehouse=warehouse,
+        role="SYSADMIN",
+    )
+
+conn = get_connection()
+
+
 # Create a cursor object
 cur = conn.cursor()
 
 try:
     # Create database if it doesn't exist
-    cur.execute("CREATE DATABASE IF NOT EXISTS demo_DB;")
+    conn.execute("CREATE DATABASE IF NOT EXISTS demo_DB;")
     print("Database 'demo_DB' ensured.")
 
     # Create schema if it doesn't exist
-    cur.execute("CREATE SCHEMA IF NOT EXISTS demo_DB.public;")
+    conn.execute("CREATE SCHEMA IF NOT EXISTS demo_DB.public;")
     print("Schema 'public' ensured in database 'demo_DB'.")
 
     # Use the created database and schema
-    cur.execute("USE DATABASE demo_DB;")
-    cur.execute("USE SCHEMA public;")
+    conn.execute("USE DATABASE demo_DB;")
+    conn.execute("USE SCHEMA public;")
 
     # Create table
     create_table_sql = """
@@ -32,9 +57,9 @@ try:
         created_at TIMESTAMP
     );
     """
-    cur.execute(create_table_sql)
+    conn.execute(create_table_sql)
     print("Table 'example_table' created successfully.")
 
 finally:
-    cur.close()
+    conn.close()
     conn.close()
