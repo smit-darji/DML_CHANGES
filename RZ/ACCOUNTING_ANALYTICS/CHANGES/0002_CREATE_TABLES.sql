702f43ad9f3622be4828484860ab6310a1658340
smit-darji <smit.softvan@gmail3.com>
2025-06-18 17:53:02 +0530

    Added

--- RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset yourname:0010-create-agile-reporting-table4
+--changeset yourname:0010-create-agile-reporting-table6
 
 CREATE TABLE IF NOT EXISTS AGILE_REPORTING.TASKS (
     ID             NUMBER       PRIMARY KEY,
