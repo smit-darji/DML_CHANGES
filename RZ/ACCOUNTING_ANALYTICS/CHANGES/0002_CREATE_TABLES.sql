24486ab8ab0e675267a959747bcf2579e3df8a29
smit-darji <smit.softvan@gmail3.com>
2025-06-19 13:24:49 +0530

    addded

--- RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -3,5 +3,8 @@
 --changeset yourname:0010-create-agile-reporting-table01
 use database DEV_RZ;
 use schema ACCOUNTING_ANALYTICS;
+use role FULL_ACCESS_ROLE;
+use warehouse SNOWFLAKE_LEARNING_WH;
+
 ALTER TABLE DEV_RZ.ACCOUNTING_ANALYTICS.TASKS
 ADD COLUMN DESCRIPTION STRING;
