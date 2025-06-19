709ee0d963bc40aaf5b1bcc44a583059e52f7d39
smit-darji <smit.softvan@gmail3.com>
2025-06-19 15:01:39 +0530

    added

--- RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/ACCOUNTING_ANALYTICS/CHANGES/0002_CREATE_TABLES.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 
---changeset yourname:0010-create-agile-reporting-table01223
+--changeset SMIT:0010-create-agile-reporting-table012231312
 use database DEV_RZ;
 use schema ACCOUNTING_ANALYTICS;
 use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.abcd (
+CREATE TABLE IF NOT EXISTS DEV_RZ.ACCOUNTING_ANALYTICS.smit (
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
