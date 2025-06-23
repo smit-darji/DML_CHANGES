5ba41c5ef4534308dd633181cc6d67a60f6e2f5c
smit-darji <smit.softvan@gmail3.com>
2025-06-23 14:41:26 +0530

    demo

--- RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
+++ RZ/AGILE_REPORTING/CHANGES/0002_CREATE_TABLES.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset DEMO:0010-create-agile-reporting-5555231212124
+--changeset DEMO:0010-create-agile-reporting-555523121212456
 
 use database DEV_RZ;
 use schema AGILE_REPORTING;
@@ -8,7 +8,7 @@ use role FULL_ACCESS_ROLE;
 use warehouse SNOWFLAKE_LEARNING_WH;
 
 
-CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.demo2334 (
+CREATE TABLE IF NOT EXISTS DEV_RZ.AGILE_REPORTING.demo23341 (
     ID             NUMBER       PRIMARY KEY,
     NAME           STRING       NOT NULL,
     STATUS         STRING,
